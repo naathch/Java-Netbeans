ecaaf338921d1a02c5389b40978e3d094f468b85
Nath <76856325+naathch@users.noreply.github.com>
2023-03-14 22:45:59 -0600

    algoritmo1.java
    Algoritmo que determine de n numero la cantidad de pares eimpares.

---
+++ .java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author naathch
+ */
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de números: ");
+        int n = input.nextInt();
+
+        int pares = 0;
+        int impares = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Ingrese el número " + i + ": ");
+            int numero = input.nextInt();
+            if (numero % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+        }
+
+        System.out.println("Cantidad de números pares: " + pares);
+        System.out.println("Cantidad de números impares: " + impares);
+    }
+}
